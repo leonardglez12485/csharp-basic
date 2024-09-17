05aff0e9ae489d04b837b1cd16df2071bf5ce57b
Leonard <leonardglez12485@gmail.com>
2024-09-16 23:43:01 -0400

    creating repo

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+var leonard = new People("Leonardo", 39);
+
+string json = JsonSerializer.Serialize(leonard);
+Console.WriteLine(json);
+
+string myJson = @"{
+    ""Name"": ""Pepe"",
+    ""Age"": 39
+}";
+
+People? person = JsonSerializer.Deserialize<People>(myJson);
+if (person != null)
+{
+    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+}
+
+public class People {
+    public string Name { get; set; }
+    public int Age {  get; set; }
+
+    public People(string name, int age){
+        Name = name;
+        Age = age;
+    }
+}
